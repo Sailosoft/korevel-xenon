aa39ee6645798d3753449dfc850c1525139a7f5c
michael.romero03 <john.michael.romero03@gmail.com>
2026-06-22 03:51:56 +0800

    Bunny: Add Modal Config support for Bunny Feature

--- src/modules/bunny/src/feature/BunnyFeature.ModalConf.ts
+++ src/modules/bunny/src/feature/BunnyFeature.ModalConf.ts
@@ -1,4 +1,5 @@
 import { BunnyConfig, BunnyModalSize } from "../Bunny.Interface";
+import { BunnyModalHeaderAction } from "../modal/BunnyModal.Interface";
 
 /**
  * Modal configuration
@@ -12,6 +13,29 @@ export class BunnyModalConfigurator<TRow, TForm> {
     } else if (typeof size === "number") {
       this.config.modalSizeWidth = size;
     }
+    return this;
+  }
+
+  /**
+   * Replace all modal header action buttons with a new set.
+   */
+  public setModalHeaderActions(
+    actions: BunnyModalHeaderAction<TRow, TForm>[],
+  ): this {
+    this.config.modalHeaderActions = actions;
+    return this;
+  }
+
+  /**
+   * Append a single modal header action button.
+   */
+  public addModalHeaderAction(
+    action: BunnyModalHeaderAction<TRow, TForm>,
+  ): this {
+    if (!this.config.modalHeaderActions) {
+      this.config.modalHeaderActions = [];
+    }
+    this.config.modalHeaderActions.push(action);
     return this;
   }
 }
